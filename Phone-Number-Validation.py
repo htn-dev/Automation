3ad93358234df11faf2254e4808b6ea383373de4
HTN <96134288+htn-dev@users.noreply.github.com>
2023-03-07 12:07:27 -0500

    Update Phone-Number-Validation.py

--- Phone-Number-Validation.py
+++ Phone-Number-Validation.py
@@ -4,5 +4,10 @@ def isPhoneNumber(text):
   for i in range(0, 3):
     if not text[i].isdecimal():
       return False
-    
+  if text[3] != '-':
+    return False
+  for i in range(4, 7):
+    if not text[i].isdecimal():
+      return False
+   
   
